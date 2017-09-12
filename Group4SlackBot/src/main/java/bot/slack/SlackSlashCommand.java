2c4d0a9113bae326dded1e2d58e1b48733007a3d
Orion Gilliam <Oriongilliam@gmail.com>
2017-09-11 21:39:06 -0400

    heroku setup

--- Group4SlackBot/src/main/java/bot/slack/SlackSlashCommand.java
+++ Group4SlackBot/src/main/java/bot/slack/SlackSlashCommand.java
@@ -68,7 +68,7 @@ public class SlackSlashCommand {
         }
 
         /** build response */
-        RichMessage richMessage = new RichMessage("The is Orion Commander!");
+        RichMessage richMessage = new RichMessage("The is Heroku Commander!");
         richMessage.setResponseType("in_channel");
         // set attachments
         Attachment[] attachments = new Attachment[1];
