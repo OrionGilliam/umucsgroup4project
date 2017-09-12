06900d2e662c4c68ce7ab217d464f1561ea81e18
Orion Gilliam <Oriongilliam@gmail.com>
2017-09-11 20:11:58 -0400

    Prepped for upload to dev

--- Group4SlackBot/src/main/java/bot/UmucSlackBotApplication.java
+++ Group4SlackBot/src/main/java/bot/UmucSlackBotApplication.java
@@ -3,7 +3,7 @@ package bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "example.jbot"})
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "bot"})
 public class UmucSlackBotApplication {
 
     /**
