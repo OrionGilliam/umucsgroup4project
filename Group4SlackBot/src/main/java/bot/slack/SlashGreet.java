1925b46f348b05c17efe0900422887a18f9ceeb5
Orion Gilliam <Oriongilliam@gmail.com>
2017-09-18 16:49:23 -0400

    fixing json file finding

--- Group4SlackBot/src/main/java/bot/slack/SlashGreet.java
+++ Group4SlackBot/src/main/java/bot/slack/SlashGreet.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -47,10 +48,16 @@ public class SlashGreet{
     @RequestMapping(value = "/greet",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public RichMessage onReceiveSlashCommand(String token, String teamId, 
-            String teamDomain, String channelId, String channelName, 
-            String userId, String userName, String command, String text, 
-            String responseUrl) {
+    public RichMessage onReceiveSlashCommand(@RequestParam("token") String token,
+                                             @RequestParam("team_id") String teamId,
+                                             @RequestParam("team_domain") String teamDomain,
+                                             @RequestParam("channel_id") String channelId,
+                                             @RequestParam("channel_name") String channelName,
+                                             @RequestParam("user_id") String userId,
+                                             @RequestParam("user_name") String userName,
+                                             @RequestParam("command") String command,
+                                             @RequestParam("text") String text,
+                                             @RequestParam("response_url") String responseUrl) {
         // validate token
         if (!token.equals(slackToken)) {
             return new RichMessage("Invalid Slack Command Token.");
