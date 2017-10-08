0006bf4e72508f2246a4940648630e18888a7a65
adattilio <Alexinator40@gmail.com>
2017-10-07 20:05:53 -0400

    Fully completed feature
    Aside from junit tests, the Scheduler feature is now completed. Only junit tests remain for the feature

--- Group4SlackBot/src/main/java/bot/slack/SlashSchedule.java
+++ Group4SlackBot/src/main/java/bot/slack/SlashSchedule.java
@@ -1,6 +1,8 @@
 package bot.slack;
 
 import bot.schedule.ScheduleEvent;
+import bot.schedule.ScheduleException;
+import me.ramswaroop.jbot.core.slack.models.Attachment;
 import me.ramswaroop.jbot.core.slack.models.RichMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,11 +11,11 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Scanner;
 
+@RestController
 public class SlashSchedule {
     private static final Logger logger = LoggerFactory.getLogger
             (SlashSchedule.class);
@@ -23,6 +25,52 @@ public class SlashSchedule {
      */
     @Value("${slashScheduleToken}")
     private String slackToken;
+
+    public static void setErrorMessage(ScheduleException exec, RichMessage
+            richMessage) {
+        Attachment errorFix = new Attachment();
+        switch (exec.getFailureReason()) {
+            case "EventFormat":
+                richMessage.setText("Error! Incorrect event name format!");
+                errorFix.setText("The event name must be within quotes. " +
+                        "Spaces must also be put between event name, date and" +
+                        " time.");
+                break;
+            case "DateFormat":
+                richMessage.setText("Error! Incorrect date format!");
+                errorFix.setText("The event date must be either a day name " +
+                        "(Sunday - Saturday), today, tomorrow, this week, " +
+                        "next week, this month, next month, this " +
+                        "year, next year, or a specific date in the " +
+                        "format mm/dd/yyyy. (ex. 04/15/2019). Spaces must " +
+                        "also be put between event name, date and " +
+                        "time.");
+
+                break;
+            case "TimeFormat":
+                richMessage.setText("Error! Incorrect time format!");
+                errorFix.setText("The event time must be in the form of " +
+                        "\"hh:mm aaa\" where h is the hour(number), m is the " +
+                        "minute(number) and aaa m is either AM or PM (ex. " +
+                        "8:42 AM). Spaces must also be put between event name, " +
+                        "date and time.");
+                break;
+            case "PastDate":
+                richMessage.setText("Error! Requested date is in the past!");
+                errorFix.setText("Please use the current date or a future " +
+                        "date!");
+                break;
+            case "AlreadyExists":
+                richMessage.setText("Error! This event has already been " +
+                        "scheduled!");
+                errorFix.setText("You may schedule a similar event if at " +
+                        "least one of the event fields (name, date, time) is " +
+                        "different.");
+        }
+        Attachment[] attachments = new Attachment[5];
+        attachments[0] = errorFix;
+        richMessage.setAttachments(attachments);
+    }
 
     /**
      * Slash Command handler. When a user types for example "/app help"
@@ -57,14 +105,20 @@ public class SlashSchedule {
         RichMessage richMessage = new RichMessage();
         Calendar calendar = Calendar.getInstance();
 
-        ArrayList<ScheduleEvent> schedule = new ArrayList<ScheduleEvent>();
-        ScheduleEvent scheduleEvent = new ScheduleEvent(text);
+
+        try {
+            ScheduleEvent scheduleEvent = new ScheduleEvent(text);
+            if (SlackBot.getEventList().contains(scheduleEvent)) {
+                throw new ScheduleException("AlreadyExists");
+            }
+            SlackBot.addScheduleEvent(scheduleEvent);
+            richMessage.setText("Your event has been scheduled: " +
+                    scheduleEvent.toString());
+        } catch (ScheduleException exec) {
+            setErrorMessage(exec, richMessage);
+        }
+
         return richMessage;
-
-
-
-
-
 
     }
 }
